de6bd70b3a40f17e15b1be893f3f3f1e41c7eb7c
Vladyslavua <vladgershman@gmail.com>
2023-07-20 20:40:46 +0200

    update:added protection for routes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import {ReactNode} from "react";
+import {auth} from "@clerk/nextjs";
+import {redirect} from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+export default async function SetupLayout({
+    children
+                                          }:{
+    children:ReactNode
+}){
+    const {userId} = auth()
+
+
+    if(!userId) return redirect('/sign-in')
+
+    const store = await prismadb.store.findFirst({
+        where:{
+            userId
+        }
+    })
+
+    if(store) return redirect(`/${store.id}`)
+
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
