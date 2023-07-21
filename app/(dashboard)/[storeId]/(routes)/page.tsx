dc25001ed29257662a8241ea5dee6a09f62bfee1
Vladyslavua <vladgershman@gmail.com>
2023-07-21 20:03:25 +0200

    "Updated Dashboard and Added Store Settings Pages"
    Added necessary imports and functions to deliver data from prismadb in the route's dashboard page. This includes displaying the store's name instead of a placeholder. Also introduced a new settings page under the route's section which return the settings form, redirects to sign-in if user isn't authenticated, or redirects to home if store doesn't exist.
    
    These changes enhance store-related functionalities and improve user experience by managing access to certain pages. They also lay the groundwork for further store settings customizations.

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,23 @@
-const DashBoardPage = () =>{
+import React from "react";
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPropsType {
+    params:{storeId:string}
+}
+
+
+const DashBoardPage:React.FC<DashboardPropsType> = async ({params}) =>{
+
+    const store = await prismadb.store.findFirst({
+        where:{
+            id:params.storeId
+        }
+    })
+
+
     return(
         <div>
-            This is Dashboard
+            Active Store : {store?.name}
         </div>
     )
 }
