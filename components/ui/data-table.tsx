37c8a56b969ddbcd7647cd63bf90216df944ba1d
Vladyslavua <vladgershman@gmail.com>
2023-08-10 18:02:06 +0200

    Added filter to data-table component and made it reusable

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -1,10 +1,17 @@
 "use client"
 
+import {Button} from '@/components/ui/button'
+import { Input } from "@/components/ui/input"
+import {useState} from "react";
+
 import {
     ColumnDef,
     flexRender,
     getCoreRowModel,
     useReactTable,
+    getPaginationRowModel,
+    ColumnFiltersState,
+    getFilteredRowModel
 } from "@tanstack/react-table"
 
 import {
@@ -19,19 +26,44 @@ import {
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
     data: TData[]
+    searchKey:string
 }
 
 export function DataTable<TData, TValue>({
                                              columns,
                                              data,
+                                             searchKey
                                          }: DataTableProps<TData, TValue>) {
+
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
+        []
+    )
+
+
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel:getPaginationRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        state:{
+            columnFilters
+        }
     })
 
     return (
+        <div>
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Search"
+                    value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
+                    onChange={(event) =>
+                        table.getColumn(searchKey)?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm"
+                />
+            </div>
         <div className="rounded-md border">
             <Table>
                 <TableHeader>
@@ -75,6 +107,25 @@ export function DataTable<TData, TValue>({
                     )}
                 </TableBody>
             </Table>
+        </div>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Previous
+                </Button>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Next
+                </Button>
+            </div>
         </div>
     )
 }
