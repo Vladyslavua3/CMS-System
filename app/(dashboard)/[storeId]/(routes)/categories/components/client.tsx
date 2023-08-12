c13ee2dc3cc24fdc0070fe139d1d7b1afecc0e78
Vladyslavua <vladgershman@gmail.com>
2023-08-12 20:09:37 +0200

    Added client component after refactoring

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,48 @@
+"use client"
+import React from 'react';
+import {Heading} from "@/components/ui/heading";
+import {Button} from "@/components/ui/button";
+import {Plus} from "lucide-react";
+import {Separator} from "@/components/ui/separator";
+import {useParams, useRouter} from "next/navigation";
+import {columns} from "@/app/(dashboard)/[storeId]/(routes)/categories/components/columns";
+import {DataTable} from "@/components/ui/data-table";
+import {ApiList} from "@/components/ui/api-list";
+import {CategoryColumn} from "@/app/(dashboard)/[storeId]/(routes)/categories/components/columns";
+
+interface CategoryClientProps {
+    data:CategoryColumn[]
+}
+
+
+const CategoryClient:React.FC<CategoryClientProps> = ({data}) => {
+
+    const router = useRouter()
+    const params = useParams()
+
+
+    const handlerForButton = () => {
+        router.push(`/${params.storeId}/billboards/new`)
+    }
+
+    return (
+        <>
+            <div className={'flex items-center justify-between'}>
+                <Heading title={`Categories (${data.length})`}
+                         description={'Manage categories for you store'}
+                />
+                <Button onClick={handlerForButton}>
+                    <Plus className={'mr-2 h-4 w-4'}/>
+                    Add New
+                </Button>
+            </div>
+            <Separator/>
+            <DataTable searchKey={'label'} columns={columns} data={data}/>
+            <Heading title={'API'} description={'API calls for Billboards'}/>
+            <Separator/>
+            <ApiList entityName={'categories'} entityIdName={'categoriesId'}/>
+        </>
+    );
+};
+
+export default CategoryClient;
