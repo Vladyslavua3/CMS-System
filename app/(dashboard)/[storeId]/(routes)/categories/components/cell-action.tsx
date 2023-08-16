ad294dfe2cb172e5c75c1349291997c4f99e65a0
Vladyslavua <vladgershman@gmail.com>
2023-08-16 21:33:20 +0200

    Added cell action for category component

--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-
 import React, {useState} from "react";
 import {
     DropdownMenu,
@@ -35,17 +34,17 @@ export const CellAction:React.FC<CellActionProps> = ({
 
     const onCopy = (id:string) => {
         navigator.clipboard.writeText(id)
-        toast.success('Billboard Id copied to the clipboard ')
+        toast.success('Category Id copied to the clipboard ')
     }
 
     const onDelete = async () => {
         try {
             setLoading(true)
-            await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
+            await axios.delete(`/api/${params.storeId}/categories/${data.id}`)
             router.refresh()
-            toast.success('Billboard deleted')
+            toast.success('Category deleted')
         }catch (e) {
-            toast.error('Make sure you removed all categories using this billboard first.')
+            toast.error('Make sure you removed all products using this category first.')
         }finally {
             setLoading(false)
             setOpen(false)
@@ -70,7 +69,7 @@ export const CellAction:React.FC<CellActionProps> = ({
                         <Copy className={'mr-2 h-4 w-4'}/>
                         Copy Id
                     </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}>
+                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/categories/${data.id}`)}>
                         <Edit className={'mr-2 h-4 w-4'}/>
                         Update
                     </DropdownMenuItem>
