30df91f2f1035068eb1ce22ca12853c2d610008a
Vladyslavua <vladgershman@gmail.com>
2023-08-27 13:18:21 +0200

    Added client component and cell action component for product

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,47 @@
+"use client"
+import React from 'react';
+import {Heading} from "@/components/ui/heading";
+import {Button} from "@/components/ui/button";
+import {Plus} from "lucide-react";
+import {Separator} from "@/components/ui/separator";
+import {useParams, useRouter} from "next/navigation";
+import {DataTable} from "@/components/ui/data-table";
+import {ApiList} from "@/components/ui/api-list";
+import {columns, ProductColumn} from "@/app/(dashboard)/[storeId]/(routes)/products/components/columns";
+
+interface ProductClientProps {
+    data:ProductColumn[]
+}
+
+
+const ProductClient:React.FC<ProductClientProps> = ({data}) => {
+
+    const router = useRouter()
+    const params = useParams()
+
+
+    const handlerForButton = () => {
+        router.push(`/${params.storeId}/products/new`)
+    }
+
+    return (
+        <>
+            <div className={'flex items-center justify-between'}>
+                <Heading title={`Products (${data.length})`}
+                         description={'Manage products for you store'}
+                />
+                <Button onClick={handlerForButton}>
+                    <Plus className={'mr-2 h-4 w-4'}/>
+                    Add New
+                </Button>
+            </div>
+            <Separator/>
+            <DataTable searchKey={'name'} columns={columns} data={data}/>
+            <Heading title={'API'} description={'API calls for Products'}/>
+            <Separator/>
+            <ApiList entityName={'products'} entityIdName={'productId'}/>
+        </>
+    );
+};
+
+export default ProductClient;
